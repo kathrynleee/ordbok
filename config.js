74acb2df8353c2fb235e35c462a266e4fe16be55
kathrynleee <50726368+kathrynleee@users.noreply.github.com>
2019-05-17 11:08:10 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+	apiKey: "AIzaSyBCeAySMZNVDldHorjNma7eoQWEpjBPSb0",
+  authDomain: "ordbok-aacc6.firebaseapp.com",
+  databaseURL: "https://ordbok-aacc6.firebaseio.com",
+}
+
+export default config;
