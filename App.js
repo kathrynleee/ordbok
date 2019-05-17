b920f7396df157954a6b71880d0e0ab28ea652e1
kathrynleee <50726368+kathrynleee@users.noreply.github.com>
2019-05-17 11:15:41 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Firebase from 'firebase';
+import config from './config';
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    Firebase.initializeApp(config.firebase);
+
+    this.state = {
+      developers: []
+    }
+  }
