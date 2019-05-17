c854e0e8e8fbec290e2d8d06b2e895c47a6ab48c
kathrynleee <50726368+kathrynleee@users.noreply.github.com>
2019-05-17 11:08:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-	apiKey: "AIzaSyBCeAySMZNVDldHorjNma7eoQWEpjBPSb0",
+  apiKey: "AIzaSyBCeAySMZNVDldHorjNma7eoQWEpjBPSb0",
   authDomain: "ordbok-aacc6.firebaseapp.com",
   databaseURL: "https://ordbok-aacc6.firebaseio.com",
 }
